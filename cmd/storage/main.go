f1390ac8321ebdcbdce9081be8859d6762e49ff0
apeks827 <apeks827@ya.ru>
2021-12-18 12:35:05 +0300

    second commit

--- cmd/storage/main.go
+++ cmd/storage/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/apeks827/storage/internal/storage"
 )
 
 func main() {
 	st := storage.NewStorage()
-	fmt.Println("it works", st)
+	file, err := st.Upload("test.txt", []byte{"hello"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("File upploaded", file)
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,18 @@
 package storage
 
+import "github.com/apeks827/storage/internal/file"
+
 type Storage struct{}
 
 func NewStorage() *Storage {
 	return &Storage{}
+}
+
+func (s *Storage) Upload(filename string, blob []byte) (*file.File, error) {
+	return file.NewFile(filename, blob)
+	//newFile, err := file.NewFile(filename, blob)
+	// if err != nil {
+	// return nil, err
+	// }
+	// return newFile
 }
